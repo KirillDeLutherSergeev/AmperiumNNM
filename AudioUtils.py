0cf387c1bd2ee37f237b246539773bc4fdfabe43
KirillDeLutherSergeev <117089118+KirillDeLutherSergeev@users.noreply.github.com>
2023-03-24 00:05:24 +0300

    Update AudioUtils.py

--- AudioUtils.py
+++ AudioUtils.py
@@ -1,12 +1,6 @@
 def save_wav(name, data, fs = 44100):
     wavfile.write(name, fs, data.flatten().astype(np.float32))
 
-def normalize(data):
-    data_max = max(data)
-    data_min = min(data)
-    data_norm = max(data_max,abs(data_min))
-    return data / data_norm
-  
 def linear_2_db(signal):
     return 20.0 * np.log10(signal)
 
@@ -21,6 +15,10 @@ def normalize_at_minus_3dB(signal):
     data_max = np.max(np.abs(signal))
     minus_3db_in_linear = 0.707
     return signal * (minus_3db_in_linear / data_max)
+
+def normalize_at_minus_6dB(signal):
+    data_max = np.max(np.abs(signal))
+    return signal * (0.5 / data_max)
 
 def calculate_rms(signal):
     rms = np.sqrt(np.mean(signal ** 2))
