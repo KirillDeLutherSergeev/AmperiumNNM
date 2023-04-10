1e9e697ce10c292fb8023a2d184fa5b16450ab30
KirillDeLutherSergeev <117089118+KirillDeLutherSergeev@users.noreply.github.com>
2023-04-10 18:49:17 +0300

    Added esr_emphasized

--- Training.py
+++ Training.py
@@ -38,6 +38,11 @@ def LowPass(x, coeff=0.85):
 def esr(target_y, predicted_y): 
     return K.sum(tf.pow(target_y - predicted_y, 2), axis=0) / (K.sum(tf.pow(target_y, 2), axis=0) + 1e-10)
 
+def esr_emphasized(target_y, predicted_y):
+    tgtY = HiPass(target_y)
+    predY = HiPass(predicted_y)
+    return K.sum(tf.pow(tgtY - predY, 2), axis=0) / (K.sum(tf.pow(tgtY, 2), axis=0) + 1e-10)
+
 def build_model(useD1=True, useC1=True, useC2=True, loss='mse', learningRate=0.008, epsilon=1.e-08, hiddenSize=16, conv1Size=128, conv2Size=2048, showInfo=False):
     # Create Sequential Model ###########################################
     clear_session()
