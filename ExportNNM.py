ecd82f8c39a6226df262b427b789e9049c7ded07
KirillDeLutherSergeev <117089118+KirillDeLutherSergeev@users.noreply.github.com>
2023-04-18 16:25:59 +0300

    Fix info length

--- ExportNNM.py
+++ ExportNNM.py
@@ -81,11 +81,11 @@ def export_model_to_nnm(filename, output_scaling_gain, dc_value, model_to_save, 
 
     header = THeader()
     header.modelType = model_type
-    header.ampType = amp_type[:10].encode('utf-8')
-    header.ampInfo = model_name[:31].encode('utf-8')
-    header.cabInfo = cab_name[:31].encode('utf-8')
-    header.addInfo1 = add_info1[:15].encode('utf-8')
-    header.addInfo2 = add_info2[:15].encode('utf-8')
+    header.ampType = amp_type[:11].encode('utf-8')
+    header.ampInfo = model_name[:32].encode('utf-8')
+    header.cabInfo = cab_name[:32].encode('utf-8')
+    header.addInfo1 = add_info1[:16].encode('utf-8')
+    header.addInfo2 = add_info2[:16].encode('utf-8')
     file.write(header)
   
     nnm = make_weights_array(model_to_save, output_scaling_gain)
